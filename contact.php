3680f88b1d60fca761e578888488686b8eb22ee0
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-08-21 13:29:37 -0600

    update contact form new php?

--- contact.php
+++ contact.php
@@ -1,15 +1,27 @@
 <?php
 
-if (isset($_POST['submit'])) {
 	$name = $_POST['name'];
 	$subject = $_POST['subject'];
-	$mailFrom = $_POST['mail'];
+	$visitor_email = $_POST['mail'];
 	$message = $_POST['message'];
 
-	$mailTo = "contact@cruejameson.com";
-	$headers = "From: ".$mailFrom;
-	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+	$email_from = 'contact@cruejameson.com';
 
-	mail($mailTo, $subject, $txt, $headers);
-	header("Location: index.php?mailsend");
-}
+	$email_subject = "New Contact Form Submission";
+
+	$email_body = "User Name: $name.\n".
+									"User Email: $visitor_email.\n".
+										"User Message: $message.\n";
+	
+	$to = "cruebeedooobie@gmail.com";
+	
+	$headers = "From: $email_from \r\n";
+
+	$headers .= "Reply-To: $visitor_email \r\n";
+
+	mail($to, $email_subject,$email_body,$headers);
+
+	header("Location: contact.html");
+	
+?>
+
