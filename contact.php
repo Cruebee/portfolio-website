2e87d1c6e057693d954923b898cb32ad91fd3723
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-08-21 12:38:07 -0600

    add injection protection and email address to send to to contact form php file

--- contact.php
+++ contact.php
@@ -1,15 +1,34 @@
 <?php
 
+function checkFields($values){
+	$injection = false;
+	for ($n=0;$n<count($values);$n++){
+		if (eregi("%0A",$values[$n]) || eregi("%0D",$values[$n]) || eregi("\\r",$values[$n]) || eregi("\\n",$values[$n])){
+			$injection = true;
+		}
+	}
+	return $injection;
+}
+
+$from = $_POST['from'];
+$email = $_POST['email'];
+$result = checkFields(Array($from, $email));
+if ($result==true){
+	die("Header injection detected!");
+}
+
 if (isset($_POST['submit'])) {
 	$name = $_POST['name'];
 	$subject = $_POST['subject'];
 	$mailFrom = $_POST['mail'];
 	$message = $_POST['message'];
 
-	$mailTo = "";
+	$mailTo = "contact@cruejameson.com";
 	$headers = "From: ".$mailFrom;
 	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
 	mail($mailTo, $subject, $txt, $headers);
 	header("Location: index.php?mailsend");
 }
+
+?>
